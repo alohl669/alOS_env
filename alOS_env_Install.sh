d541a1294b488cb30c21ae5731efbb7773ad5d74
alohl669 <alohl669@gmail.com>
2020-11-30 23:46:52 +0100

    scripts - bash regex fixed

--- alOS_env_Install.sh
+++ alOS_env_Install.sh
@@ -4,7 +4,7 @@ mkdir -p $HOME/.alOS/pictures
 cd $HOME/.alOS
 echo -n "You need to install some tools. Do you want to install it?[Y/n]: "
 read installGitAnswer
-if [[ $installGitAnswer = ['','Y','y'] ]]; then
+if [[ $installGitAnswer = [Yy] || $installGitAnswer = '' ]]; then
     sudo apt-get install -y git net-tools
 else
     echo "ok, bye"
