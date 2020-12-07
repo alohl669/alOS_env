b1adee4681396b6cc6d1640b24cb2cc3498405d0
alohl669 <alohl669@gmail.com>
2020-12-07 22:43:01 +0100

    ETH

--- install_files/bin/ethernet_status.sh
+++ install_files/bin/ethernet_status.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo -e "%{F#BABDB6} %{F#000000}$(/usr/sbin/ifconfig eth0 2>/dev/null| grep "inet " | awk '{print $2}')%{u-}"
+echo -e "%{F#BABDB6} %{F#000000}$(/usr/sbin/ifconfig ETHIFACE 2>/dev/null| grep "inet " | awk '{print $2}')%{u-}"
