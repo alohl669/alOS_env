a1d7d3d918f2e97e6f63ea62b20fd92fa97e8097
alohl669 <alohl669@protonmail.com>
2021-01-27 01:53:38 +0100

    zsh added

---
+++ scripts/zsh_install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt-get install -y zsh
+cd $HOME/
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+zsh
+# en algun momento hay que instalarlo en root
+su
+cd $HOME/
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+zsh
