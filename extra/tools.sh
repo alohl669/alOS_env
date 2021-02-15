601c7b2e0953de111ab699aa363c8d0fb057873a
alohl669 <alohl669@protonmail.com>
2021-02-15 19:05:12 +0100

    fixed install bat

--- extra/tools.sh
+++ extra/tools.sh
@@ -1,7 +1,7 @@
 function installBat () {
     cd /tmp/
     wget https://github.com/sharkdp/bat/releases/download/v0.17.1/bat_0.17.1_amd64.deb
-    dpkg -i bat_0.17.1_amd64.deb
+    sudo dpkg -i bat_0.17.1_amd64.deb
 }
 function installFileManager () {
     # Gestor de archivos(zip, war, rar, tar..)
