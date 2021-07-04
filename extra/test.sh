85088da5e3b3691701810f5a75222c624c5bd6c2
Alvaro Hernandez <alohl669@gmmail.com>
2021-07-04 02:01:31 +0200

    adding first test for trackpad compatibility. First script using ubuntu 20.04

---
+++ extra/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Trackpad haptic compatibility(Tested on ubuntu 20.04.2)
+cat>/usr/share/X11/xorg.conf.d/69-nice.conf<<EOF
+Section "InputClass"
+    Identifier "touchpad overrides"
+    MatchDriver "libinput"
+    Option "ClickMethod" "clickfinger"
+    Option "DisableWhileTyping" "true"
+    Option "MiddleEmulation" "true"
+    Option "Tapping" "true"
+EndSection
+EOF
