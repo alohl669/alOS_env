d43612c75e2e721733820eb855f9dc75837e2065
Alvaro Hernandez <alohl669@gmmail.com>
2021-07-14 20:55:31 +0200

    fixes

--- install_files/bin/ethernet_status.sh
+++ install_files/bin/ethernet_status.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ifaceAdapter=($(ifconfig -a | grep -v "lo:" | grep RUNNING | cut -d ':' -f1))
+ifaceAdapter=($(/usr/sbin/ifconfig -a | grep -v "lo:" | grep RUNNING | cut -d ':' -f1))
 ifaceAdapter=${ifaceAdapter[$(expr ${#ifaceAdapter[@]} - 1)]}
 if [[ $ifaceAdapter = "e"* ]]; then
 	echo -e "%{F#BABDB6} %{F#000000}$(/usr/sbin/ifconfig $ifaceAdapter 2>/dev/null| grep "inet " | awk '{print $2}')%{u-}"
